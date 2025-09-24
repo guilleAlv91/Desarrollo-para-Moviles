98a2b0ba835c10924b0bb2aeead06286983f6e11
unknown <basteiroabril@gmail.com>
2025-09-24 18:28:19 -0300

    agrego boton reutilizable y opcion de registro

---
+++ GEORRHH/components/GradientButton.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, Text, Pressable, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from '../utils';
+
+interface GradientButtonProps {
+    title: string;
+    onPress: () => void;
+    isEnabled: boolean;
+}
+
+export const GradientButton: React.FC<GradientButtonProps> = ({ title, onPress, isEnabled }) => {
+    return (
+        <Pressable onPress={onPress} disabled={!isEnabled} style={styles.buttonWrapper}>
+            {isEnabled ? (
+                <LinearGradient
+                    colors={[colors.primary, colors.secondary]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={styles.fullGradientButton}
+                >
+                    <Text style={styles.loginTextEnabled}>{title}</Text>
+                </LinearGradient>
+            ) : (
+                <LinearGradient
+                    colors={[colors.primary, colors.secondary]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={styles.gradientBorder}
+                >
+                    <View style={styles.loginButtonDisabled}>
+                        <Text style={styles.loginTextDisabled}>{title}</Text>
+                    </View>
+                </LinearGradient>
+            )}
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonWrapper: {
+        width: 280,
+        borderRadius: 25,
+        overflow: 'hidden',
+        marginTop: 20,
+    },
+    fullGradientButton: {
+        paddingVertical: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loginTextEnabled: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    gradientBorder: {
+        padding: 2,
+        borderRadius: 25,
+    },
+    loginButtonDisabled: {
+        backgroundColor: colors.backgroundColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 13,
+        borderRadius: 23,
+    },
+    loginTextDisabled: {
+        fontSize: 18,
+        color: "gray",
+        fontWeight: 'bold',
+    }
+});
