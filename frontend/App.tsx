87b11c638582788a03c91835a3d183275c864340
unknown <basteiroabril@gmail.com>
2025-11-17 19:38:14 -0300

    Agrego styled component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Root from './app/root';
 import AuthProvider from './shared/context/AuthContext/auth-provider';
+import { MenuProvider } from 'react-native-popup-menu';
 // import * as SplashScreen from 'expo-splash-screen'
 
 // SplashScreen.preventAutoHideAsync()
@@ -15,11 +16,13 @@ import AuthProvider from './shared/context/AuthContext/auth-provider';
 export default function App() {
     return (
         <SafeAreaProvider>
-            <NavigationContainer>
-                <AuthProvider>
-                    <Root />
-                </AuthProvider>
-            </NavigationContainer>
+            <MenuProvider>
+                <NavigationContainer>
+                    <AuthProvider>
+                        <Root />
+                    </AuthProvider>
+                </NavigationContainer>
+            </MenuProvider>
         </SafeAreaProvider>
     );
 }
