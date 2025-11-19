7152d39a703bc1458882cad9732b783bae93204c
unknown <basteiroabril@gmail.com>
2025-11-19 19:49:48 -0300

    Agrego geolocalizacion para corroborar ubicacion del empleado

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,7 +21,10 @@ import * as Joi from 'joi';
                 API_PORT: Joi.number().default(3000),
                 JWT_SECRET: Joi.string().required(),
                 JWT_EXPIRES_IN: Joi.string().default('3600s'),
-                QR_TEXT: Joi.string().required()
+                QR_TEXT: Joi.string().required(),
+                OFFICE_LAT: Joi.number().required(),
+                OFFICE_LONG: Joi.number().required(),
+                MAX_DISTANCE_METERS: Joi.number().default(100),
             }),
             envFilePath: '.env',
         }),
